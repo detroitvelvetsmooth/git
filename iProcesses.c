ebcdc647f2efd0e24ed38e153381db7bad00765b
avopni <andy_vopni@hotmail.com>
2010-11-09 12:44:01 -0800

    Touched up a bit

--- iProcesses.c
+++ iProcesses.c
@@ -44,50 +44,47 @@ void iProcessAlarm(){
 }
 
 void iProcessCRT(){
-
-	if((*CRTSharedMemPointer).completedFlag == 1){//== 1 or 0?? Ask Andy
+	if((*CRTSharedMemPointer).completedFlag == 1){//Indicates CRT has completed copying. Means empty buffer.
 	
 		struct messageEnvelope* env = NULL;
 		env = k_receive_message();//primitive name
 	
 		if (env != NULL){ //which it should always be the case
-
-		int i;
-		while(env->data[i] != NULL){//how can I extract data length? Print MAXCHAR everytime?TODO while loop logic
+		    int i;
+		    while(env->data[i] != NULL){//how can I extract data length? Print MAXCHAR everytime?TODO while loop logic
 			(*CRTSharedMemPointer).data[i] = env->data[i];
 			i++;
-		}
-	
-	env->messageType = DISPLAY_ACK; //enumerated?
-	k_send_message(env->PIDSender,env);//or do we dealloacate msg?
-	(*CRTSharedMemPointer).bufferLength = i;
-	(*CRTSharedMemPointer).completedFlag = 0;//or 1??
-	 }
-   }
+		    }
+	            env->messageType = MSGTYPEACK;
+	            k_send_message(env->PIDSender,env);//or do we dealloacate msg?
+	            (*CRTSharedMemPointer).bufferLength = i;
+	            (*CRTSharedMemPointer).completedFlag = 0; //Indicate to UNIXCRT that buffer is loaded
+	        }
+         }
 }
 
 void iProcessKeyboard(){
 
-if((*keyboardSharedMemPointer).completedFlag == 1){
+if((*keyboardSharedMemPointer).completedFlag == 1)
+{
 	messageEnvelope* env = NULL;
 	env = k_receive_message();
-		
 		if (env != NULL){//should we loop til inbox is empty? CCI can only ever send 1 request,then it gets blocked
-		int bufferLength = (*keyboardSharedMemPointer).bufferLength;
-		for(int i = 0; i < bufferLength; i++){//assume env->data is a char[MAXCHAR]?
-		env->data[i] = (*keyboardSharedMemPointer).data[i];
+		    int bufferLength = (*keyboardSharedMemPointer).bufferLength;
+		    for(int i = 0; i < bufferLength; i++){//assume env->data is a char[MAXCHAR]?
+		       env->data[i] = (*keyboardSharedMemPointer).data[i];
+	    	    }
+		    env->messageType = MSGCONSOLEINPUT;
+		    k_send_message(env->PIDSender, env);
+		    (*keyboardSharedMemPointer).completedFlag = 0;
+		    (*keyboardSharedMemPointer).bufferLength = 0;
+		    //printf("Found in Data Buffer: %s\n",dataBuffer);
 		}
-		env->messageType = CONSOLE_INPUT; //enumerated?
-		k_send_message(env->PIDSender, env);
-		(*keyboardSharedMemPointer).completedFlag = 0;
-		(*keyboardSharedMemPointer).bufferLength = 0;
-		//printf("Found in Data Buffer: %s\n",dataBuffer);
-		}
-	}else{
-		k_release_message_env(env);//if mem not ready, ignore env; will never happen
-		}
+	}
+else{
+	k_release_message_env(env);//if mem not ready, ignore env; will never happen
 }
-
+}
 
 
 
