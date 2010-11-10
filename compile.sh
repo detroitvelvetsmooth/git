551b36b6e39c54e30a8eca08959d6ff6e63dc2a6
Chuyzoz <arrowhead06@gmail.com>
2010-11-10 16:17:49 -0500

    fixed compile.sh to actually compile and not mess it up

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Start Removing..
-rm ./Main.c ./startInitializeProcess.c ./startMessageEnvelope.c ./startSignalHandling.c ./startForkAuxillary.c ./iProcesses.c ./kPrimitives.c ./UserAPI.c ./UserProcesses.c 
+rm ./Main.o ./startInitializeProcess.o ./startMessageEnvelope.o ./startSignalHandling.o ./startForkAuxillary.o ./iProcesses.o ./kPrimitives.o ./UserAPI.o ./UserProcesses.o 
 rm -Wall -W -c ./UNIXcrt.c 
 rm -Wall -W -c ./UNIXkbd.c 
 echo ...Done Removing
