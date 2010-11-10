1107002640d0209558d55c7f31c273aa453c2b88
avopni <andy_vopni@hotmail.com>
2010-11-10 11:16:28 -0800

    Modified ProcessC

--- UserProcesses.c
+++ UserProcesses.c
@@ -5,13 +5,8 @@
 
 //////// FUNCTION PROTOTYPES AND STRUCTS///////////////////
 
-struct CQueue{
-    struct messageEnvelope* ptrMessageInboxHead;
-    struct messageEnvelope* ptrMessageInboxTail;
-};
-
-struct CQueue* LocalQueue; // TODO - FIND A PROPER WAY TO INITIALIZE. 
-
+struct messageEnvelope* ProcessCQHead;
+struct messageEnvelope* ProcessCQTail;
 void CEnqueue(struct CQueue* tempQ, struct messageEnvelope* newEnv);
 struct messageEnvelope* CDequeue(struct CQueue* tempQ);
 int isEmpty(struct CQueue* tempQ);
@@ -62,55 +57,47 @@ void ProcessB(){
      printf("\nProcess B code reached a point where it shouldn't. ERROR. \n");
 }
 
-void ProcessC(){ //TODO THIS FUNCTION IS INCOMPLETE.
+void ProcessC(){
      //TODO: Perform Initialization of local message queue
+     QHead = NULL;
+     QTail = NULL;
      struct messageEnvelope* CEnv;
      int status, count;
+     
      //Start infinite loop.
      do{
         //Check local queue for waiting messages. Dequeue a waiting message, otherwise receive message.
         if(isEmpty(LocalQueue)==0) 
             CEnv = receive_message();
         else
-            CEnv = CDequeue(LocalQueue); //TODO: Fill in Proper function name.
+            CEnv = CDequeue();
         //Check messagetype to make sure its from process B.
        
-        if (CEnv->messageType == (int)MSGTYPECOUNT){
+        if (CEnv->messageType == MSGTYPECOUNT){
             //If message content is devisable by 20 then continue.
             count = atoi(CEnv->messageText);
-            if ((count%20) == 0){ //TODO: Check syntax on how this works.
+            if ((count%20) == 0){ 
               
-               CEnv->messageText[0] = 'P';
-               CEnv->messageText[1] = 'r';
-               CEnv->messageText[2] = 'o';
-               CEnv->messageText[3] = 'c';
-               CEnv->messageText[4] = 'e';
-               CEnv->messageText[5] = 's';
-               CEnv->messageText[6] = 's';
-               CEnv->messageText[7] = ' ';
-               CEnv->messageText[8] = 'C';
-               CEnv->messageText[9] = '\0';
+               strcpy(CEnv->messageText, "Process C\0";
                
                status = send_console_chars(CEnv);
                if (status != 1)
-                  printf("\nsend_console_chars failed for Process C. ERROR.\n");
+                  printf("\nSend_console_chars failed for Process C. ERROR.\n");
                CEnv = receive_message();
                //Wait for output acknowledgement. Enqueue all non-acknowledge messages to local queue.
-               while(CEnv->messageType != (int)MSGTYPEACK){
-                   CEnqueue(LocalQueue, CEnv); //TODO: Fill in Proper Function Name.
+               while(CEnv->messageType != MSGTYPEACK){
+                   CEnqueue(CEnv); //TODO: Fill in Proper Function Name.
                    CEnv = receive_message();
                }
                //Request Sleep.
-               CEnv->messageType = (int)MSGTYPESLEEP;
-               CEnv->messageText[0] = '1';
-               CEnv->messageText[1] = '0';
-               CEnv->messageText[2] = '\0';
-               requestSleep(CEnv); //TODO: Fill in Proper function name.
+               CEnv->messageType = MSGTYPESLEEP;
+               strcpy(CEnv->messageText, "10\0");
+               request_delay(CEnv); //TODO: Fill in Proper function name.
                //Once done sleeping, enqueue all messages received during sleep to the local queue.
                do{
                   CEnv = receive_message();
-                  if (CEnv->messageType == (int)MSGTYPEWAKEUP)
-                     break;
+                  if (CEnv->messageType == MSGTYPEWAKEUP)
+                      break;
                   LocalEnqueue(CEnv);
                }while(1);
             }
@@ -224,57 +211,47 @@ void CCI()
 }
 
 ///////////////////// USER PROCESSES HELPER FUNCTIONS ////////////
-int isEmpty(struct CQueue* tempQ){
-    if(tempQ == NULL)
-        return -1;
-    else if(tempQ->ptrMessageInboxHead == NULL)
-        return 1;
+int isCQEmpty(){
+    if(ProcessCQHead == NULL)
+       return 1;
     else
-        return 0;
+       return 0;
 }
-struct messageEnvelope* CDequeue(struct CQueue* tempQ){
+struct messageEnvelope* CDequeue(){
     struct messageEnvelope* returnEnv;
-    //NULL Queue Case
-    if (tempQ == NULL){
-       printf("CDequeue: No Queue passed\n");
-	   return returnEnv;
-    }
     //Empty Queue Case
-    else if(tempQ->ptrMessageInboxHead==NULL){
-       printf("CDequeue: Dequeue requested on empty Queue.\n");
+    if(ProcessCQHead==NULL){
+       printf("CDequeue: Dequeue requested, but Process C's Q is empty.\n");
        return returnEnv;
     }
     //Standard Case
     else{
-       returnEnv = tempQ->ptrMessageInboxHead;
+       returnEnv = ProcessCQHead;
        //Queue of length 1 case
-       if(tempQ->ptrMessageInboxHead == tempQ->ptrMessageInboxTail)
-          tempQ->ptrMessageInboxTail = NULL;
+       if(ProcessCQHead == ProcessCQTail)
+          ProcessCQTail = NULL;
        //Move the head pointer down the line
-       tempQ->ptrMessageInboxHead = returnEnv->ptrNextMessage;
+       ProcessCQHead = returnEnv->ptrNextMessage;
        returnEnv->ptrNextMessage = NULL;
        return returnEnv;
     }
 }
 
 
-void CEnqueue(struct CQueue* tempQ, struct messageEnvelope* newEnv){
-     //Null Q case.
-     if (tempQ == NULL)
-        printf("\nA NULL Queue was passed to CEnqueue. ERROR.\n");
-             
+void CEnqueue(struct messageEnvelope* newEnv){    
      newEnv->ptrNextMessage = NULL;
      //Empty Q case
-     if (tempQ->ptrMessageInboxHead == NULL){
-        tempQ->ptrMessageInboxHead = newEnv;
-        tempQ->ptrMessageInboxTail = newEnv;
+     if (ProcessCQHead == NULL){
+        ProcessCQHead = newEnv;
+        ProcessCQTail = newEnv;
      }
      //Standard Q Case
      else{
-        tempQ->ptrMessageInboxTail->ptrNextMessage = newEnv;
-        tempQ->ptrMessageInboxTail = newEnv;
+        ProcessCQTail->ptrNextMessage = newEnv;
+        ProcessCQTail = newEnv;
      }
 }
+
 
 
 
