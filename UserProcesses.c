dd101c55c4f73017312ab789d28cb45825e91a38
bacousin <jamin_cousins@hotmail.com>
2010-11-09 23:39:23 -0500

    added initial basic CCI to userProcesses.c

--- UserProcesses.c
+++ UserProcesses.c
@@ -1,4 +1,6 @@
 #include "publicAPI.h"
+#include <stdio.h>
+#include <string.h>
 
 
 //////// FUNCTION PROTOTYPES AND STRUCTS///////////////////
@@ -181,3 +183,92 @@ void CEnqueue(struct CQueue* tempQ, struct messageEnvelope* newEnv){
         tempQ->ptrMessageInboxTail = newEnv;
      }
 }
+
+void CCI()
+{
+	int hour, min, sec;
+	int newPri, PID;
+	char tempChar;
+	struct messageEnvelope *temp;
+	temp = request_message_env(); //should we request and release an envelope on every cycle?
+	
+	while(1)
+	{
+	strcpy(temp->messageText, "CCI:"); 
+	send_message((int)(PIDiProcessCRT), temp);
+	temp = receive_message();
+	
+	get_console_chars(temp);
+	temp = receive_message(); //assuming KB iProcess sends evn back to process
+	
+	if(strcmp(temp->messageText, " ")==0)
+	{
+		get_console_char(temp);
+		temp = receive_message();
+	}
+	elseif(strcmp(temp->messageText, "s")==0)
+	{
+		send_massage((int)(PIDUserProcessA), temp);
+		release_processor(); 
+	}
+	elseif(strcmp(temp->messageText, "ps")==0)
+	{
+		request_process_status(temp);
+		send_console_chars(temp); //we need to figure out where the status info get put into a table format. Here??
+		temp = receive_message();
+	}
+	
+	elseif(strcmp(temp->messageText, "cd")==0)
+	{
+		//change flag of wall clock to send time to CRT every second
+	}
+	elseif(strcmp(temp->messageText, "ct")==0)
+	{
+		//change flag of wall clock to stop sending time to CRT
+	}
+	elseif(temp->messageText[0] == 'c') 
+	{
+		sscanf(temp->messageText, "%c %d:%d:%d", &tempChar, &hour, &min, &sec); //needs to check if correct number of items passed
+		if(hour < 0 || hour >=24 || min < 0 || min >=60 || sec < 0 || sec >= 60)
+		{
+			strcpy(temp->messageText, "Illegal Time Entered");
+			send_console_chars(temp);
+			temp = receive_message();
+		}
+		else
+		{
+			wall_clock(hour,min,sec); //We should make wall clock accept these values 
+		}
+	}
+	elseif(strcmp(temp->messageText, "b")==0)
+	{
+		get_trace_buffer(temp);
+		send_console_chars(temp);
+		temp = receive_message();
+	}
+	elseif(strcmp(temp->messageText, "t")==0)
+	{
+		terminate();
+	}
+	elseif(temp->messageText[0] == 'n')
+	{
+		sscanf(temp->messageText, "%c &d &d", &tempChar, &newPri, &PID); //needs to check if correct number of items passed
+		int check = change_priority(newPri, PID);
+		if(check == -1)
+		{
+			strcpy(temp->messageText, "Priority Change Failed");
+			send_console_chars(temp);
+			temp = receive_message();
+		}
+	}
+	else
+	{
+		strcpy(temp->messageText, "Illegal Command");
+		send_console_chars(temp);
+		temp = receive_message();
+	}
+	}
+}
+	
+
+
