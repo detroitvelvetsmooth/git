46c2da54e93d71c265aedc8511d95afe28697227
chuyzoz <arrowhead06@gmail.com>
2010-11-28 17:54:18 -0500

    removed unnecessary .sh files. The rtx has my seal of approval now

--- execute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo Start Executing...
-./primitiveRTX.o
-echo ...Done Executing!
-
--- setPermissions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Changing Folder Permissions...
-chmod 770 /home/jazozaya/RTX
-echo Changing User File Permissions...
-chmod 770 /home/jazozaya/RTX/*
-
